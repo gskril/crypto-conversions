40c9d86cd1b14352edf16bbd36f03a94017c738c
Greg <gskril@gmail.com>
2021-11-20 01:11:05 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Crypto Conversions
+
+Easily convert USD into the price of the top 20 cryptocurrencies, along with fractional units of Bitcoin and Ethereum.
---
+++ script.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", async () => {
+	function getCryptoData() {
+		return axios
+			.get('https://min-api.cryptocompare.com/data/pricemulti?tsyms=USD&fsyms=BTC,ETH,BNB,SOL,ADA,XRP,DOT,DOGE,SHIB,AVAX,LUNA,LTC,CRO,UNI,LINK,ALGO')
+			.then((res) => {
+				return res.data
+		})
+	}
+
+	const data = await getCryptoData()
+	const form = document.querySelector("form")
+	const input = document.getElementById("input")
+	const inputLabel = document.querySelector("label[for='input'")
+	const output = document.getElementById("output")
+	const outputLabel = document.querySelector("label[for='output'")
+	const radio = document.getElementById("crypto")
+
+	function updateNumbers() {
+		const selected = radio.options[radio.selectedIndex].value
+
+		if (selected === "wei") {
+			inputLabel.innerHTML = "ETH"
+			outputLabel.innerHTML = "Wei"
+			output.value = input.value * 1000000000000000000
+		} else if (selected === "gwei") {
+			inputLabel.innerHTML = "ETH"
+			outputLabel.innerHTML = "Gwei"
+			output.value = input.value * 1000000000
+		} else if (selected === "satoshi") {
+			inputLabel.innerHTML = "BTC"
+			outputLabel.innerHTML = "Satoshis"
+			output.value = input.value * 100000000
+		} else {
+			inputLabel.innerHTML = "USD"
+			outputLabel.innerHTML = selected
+			output.value = (data[selected].USD * input.value).toFixed(2)
+		}
+	}
+
+	form.addEventListener("change", updateNumbers)
+	form.addEventListener("keyup", updateNumbers)
+})
